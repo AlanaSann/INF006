f1a61f637e0db03dd89ff1834ed35178e9083976
AlanaSann <109287005+AlanaSann@users.noreply.github.com>
2022-08-29 19:31:02 -0300

    correção

--- RGLP/Questão7.c
+++ RGLP/Questão7.c
@@ -5,21 +5,22 @@
 // repetidos na saída
 #include <stdio.h>
 int main(void){
-  int conj1[10], conj2[20], i, j, igual [10], cont =0;
+  int conj1[2], conj2[4], i, j, igual [2], cont =0;
   printf("Digite o primeiro conjunto de números: ");
-  for(i=0;i<10;i++)
+  for(i=0;i<2;i++)
     scanf("%d",&conj1[i]);
   printf("Digite o segundo conjunto de números: ");
-  for(i=0;i<20;i++)
+  for(i=0;i<4;i++)
     scanf("%d",&conj2[i]);
-  for(i=0; i<10; i++)
-    for(j=0;j<20;j++)
+  for(i=0; i<2; i++)
+    for(j=0;j<4;j++)
       if(conj2[j]==conj1[i] && conj1[i] != igual[i]){
         igual[cont] = conj1[i];
-        printf("%d", igual[cont]);
+        // printf("%d", igual[cont]);
         cont++;
         }
   printf("Números iguais: \n");
   for(i=0; i<cont; i++)
-    printf("%d\n", igual[i]);
+    if(igual[i]!=igual[i-1])
+      printf("%d\n", igual[i]);
 }
